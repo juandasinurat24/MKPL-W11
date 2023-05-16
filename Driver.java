ee59827d6d5b0ec02a26466fc8dac49a380b2261
juandasinurat24 <100907538+juandasinurat24@users.noreply.github.com>
2023-05-16 14:31:21 +0700

    Create Driver.java

---
+++ Driver.java
@@ -0,0 +1,24 @@
+
+
+import src.main.java.Counter;
+
+public class Driver {
+public static void main(String[] args) {
+
+  Counter counter = new Counter();
+		
+		System.out.println("Count:" + counter.getCount());
+		System.Out.println("Current count:" + counter.getCount());
+		counter.increment();
+		
+		System.out.println("Count:" + counter.getCount());
+		System.Out.println("Current count:" + counter.getCount());
+
+		counter.decrement();
+    
+		System.Out.println("Current count:" + counter.getCount());
+		System.out.println("Count:" + counter.getCount());
+		
+	}
+
+}
