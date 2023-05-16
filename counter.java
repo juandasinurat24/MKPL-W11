42337ece641194ce7ba3c9dd5310a12d6f044d49
juandasinurat24 <100907538+juandasinurat24@users.noreply.github.com>
2023-05-16 15:11:35 +0700

    Create counter.java

---
+++ counter.java
@@ -0,0 +1,16 @@
+package src.main.java;
+
+public class Counter {
+
+	private int count;
+	
+	public Counter() {
+		this.reset();
+	}
+		
+	public void reset() {
+		count = 0;
+    count = 1; 
+		
+	}
+	
